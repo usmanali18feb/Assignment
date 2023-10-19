91fc8061cabab39be07b9a55257fed88b2e70c0a
usmanali18feb <148448870+usmanali18feb@users.noreply.github.com>
2023-10-19 16:31:01 +0500

    Create tests_routes.py

---
+++ tests/tests_routes.py
@@ -0,0 +1,38 @@
+import json
+from django.urls import reverse
+from django.test import TestCase
+
+class RoutesTestCase(TestCase):
+
+    def setUp(self):
+        # Set up test data (create instances in the database if needed)
+        pass
+
+    def test_read_route(self):
+        # Test reading data from a specific route
+        pass
+
+    def test_update_route(self):
+        # Test updating data through a specific route
+        pass
+
+    def test_delete_route(self):
+        # Test deleting data through a specific route
+        pass
+
+    def test_list_all_route(self):
+        # Test listing all data from a specific route
+        pass
+
+    def test_list_by_name_route(self):
+        # Test listing data by name from a specific route
+        pass
+
+    def test_list_by_category_route(self):
+        # Test listing data by category from a specific route
+        pass
+
+    def test_list_by_availability_route(self):
+        # Test listing data by availability from a specific route
+        pass
+
